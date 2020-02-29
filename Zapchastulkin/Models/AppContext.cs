a631f40496494a8fe25cbecf886c0907f4c1b499
timchnk <timchda@gmail.com>
2020-02-29 19:01:28 +0200

    Created simple crud

---
+++ Zapchastulkin/Models/AppContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Zapchastulkin.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Product> Products { get; set; }        
+    }
+}
---
+++ Zapchastulkin/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Zapchastulkin.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public int Price { get; set; }  
+    }
+}

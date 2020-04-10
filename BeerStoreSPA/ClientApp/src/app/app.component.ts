413425afc7bc915ea5bef03cae8c44218ba79095
timchnk <timchda@gmail.com>
2020-04-10 10:52:24 +0300

    footer fixed

--- BeerStoreSPA/ClientApp/src/app/app.component.ts
+++ BeerStoreSPA/ClientApp/src/app/app.component.ts
@@ -1,7 +1,8 @@
 ﻿import { Component } from '@angular/core';
-
+ 
 @Component({
     selector: 'app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent { }
